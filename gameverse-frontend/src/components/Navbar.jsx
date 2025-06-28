a91852df62fdf9720cdc609acd5cafd7b7cf783d
Sebastián Gomez <sebastianga48@gmail.com>
2025-06-28 16:20:36 -0600

    se movio img a carpeta public

--- gameverse-frontend/src/components/Navbar.jsx
+++ gameverse-frontend/src/components/Navbar.jsx
@@ -4,7 +4,9 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="/">GameVerse</a>
+                <a className="navbar-brand" href="/">
+                    <img src="/assets/img/LOGO.png" alt="GameVerse" height="40" />
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                     aria-label="Toggle navigation">
